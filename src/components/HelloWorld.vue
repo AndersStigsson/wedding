a755e6ffc2882835a4fa4d5a8d3512cb08fa7304
Anders Stigsson <github@skafteresort.se>
2021-08-28 19:19:33 +0200

    Fixes with form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>Testing</p>
+    <h2>
+      Vad roligt att Ni vill komma på vårt bröllop!
+    </h2>
+    <p>
+      Vänligen fyll i formuläret och klicka på skicka in.
+    </p>
+    <form id="send-form">
+      <p>
+        Hur många är Ni?
+        <select v-model="amount">
+          <option value="1">
+            En
+          </option>
+          <option value="2">
+            Två
+          </option>
+          <option value="3">
+            Tre
+          </option>
+          <option value="4">
+            Fyra
+          </option>
+        </select>
+      </p>
+      <div
+        v-for="n in Number(amount)"
+        :key="n"
+        :index="n"
+      >
+        <p>
+          Förnamn:
+          <input
+            v-model="fname"
+            type="text"
+          >
+          Efternamn:
+          <input
+            v-model="lname"
+            type="text"
+          >
+        </p>
+      </div>
+      <p>
+        Behöver Ni parkeringsplats?
+        <input
+          v-model="parking"
+          type="checkbox"
+        >
+      </p>
+      <p>
+        Planerar Ni att hålla tal eller framföra ett spex?
+        <input
+          v-model="speech"
+          type="checkbox"
+        >
+      </p>
+
+      <SendButton text="Skicka In" />
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import SendButton from './SendButton.vue'
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  components: {
+    SendButton
   },
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      fname: '',
+      lname: '',
+      amount: '1',
+      parking: false,
+      speech: false
+    }
+  }
 })
 </script>
 
@@ -31,5 +104,8 @@ li {
 }
 a {
   color: #42b983;
+}
+form {
+  align-content: left;
 }
 </style>
---
+++ src/components/SendButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button>
+    {{ text }}
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SendButton',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+button {
+    background-color: green;
+    color: white;
+    border-radius: 5px;
+}
+
+</style>
