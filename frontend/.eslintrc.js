6ce0058cfa5c37e17b5baf0c71d1a63b713244dd
Anders Stigsson <github@skafteresort.se>
2022-03-06 16:22:47 +0100

    fix

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,17 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/recommended',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'comma-dangle': ['error', 'never']
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
-}
+};
