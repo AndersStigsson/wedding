54e55571c9037ea46d864123581843415e7e9963
Anders Stigsson <github@skafteresort.se>
2022-03-06 16:30:51 +0100

    Routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import Toastmasters from '../components/ToastMasters.vue'
+import Couple from '../components/Couple.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/toastmasters',
     name: 'Toastmasters',
     component: Toastmasters
+  },
+  {
+    path: '/couple',
+    name: 'Couple',
+    component: Couple
   },
   {
     path: '/:catchAll(.*)',
