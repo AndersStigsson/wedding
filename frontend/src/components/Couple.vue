db34a726c69650306abc845b6326b75ffb26be08
Anders Stigsson <github@skafteresort.se>
2022-03-28 08:12:43 +0200

    Fixed table on mobile

--- frontend/src/components/Couple.vue
+++ frontend/src/components/Couple.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col container max-w-lg mt-10 mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow"
+    class="flex flex-col container max-w-lg mt-10 mx-auto w-full bg-white dark:bg-gray-800 rounded-lg shadow"
   >
     <table class="table table-responsive table-auto">
       <thead>
